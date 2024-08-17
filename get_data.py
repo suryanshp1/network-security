2a3b65e5388d622a727676f9b9d29dba6da09a18
suryanshp1 <suryanshp1@gmail.com>
2024-08-18 00:54:14 +0530

    feat: get_data code

---
+++ get_data.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import json
+from dotenv import load_dotenv
+import certifi
+import pandas as pd
+import numpy as np
+import pymongo
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+
+load_dotenv()
+
+
+class NetworkDataExtract:
+    def __init__(self) -> None:
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+    def csv_to_json_converter(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def pushing_data_to_mongodb(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    if __name__ == "__main__":
+        pass
