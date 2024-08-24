36d7e0d028598d3ab1bd1a30adf61d7b023485d2
suryanshp1 <suryanshp1@gmail.com>
2024-08-24 14:11:42 +0530

    feat: Added constant

---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+"""
+defining common constant variable for the training pipeline
+"""
+
+TARGET_COLUMN: str = "Result"
+PIPELINE_NAME: str = "NetworkSecurity"
+ARTIFACT_DIR: str = "Artifacts"
+FILE_NAME: str = "NetworkData.csv"
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+PREPROCESSING_OBJECT_FILE_NAME: str = "preprocessing.pkl"
+MODEL_FILE_NAME: str = "model.pkl"
+SCHEMA_FILE_PATH: str = os.path.join("data_schema", "schema.yaml")
+SCHEMA_DROP_COLS: str = "drop_columns"
+
+SAVED_MODELS_DIR = os.path.join("saved_models")
+"""
+Data ingestion related constants start with DATA_INGESTION VAR name
+"""
+
+DATA_INGESTION_COLLECTION_NAME: str = "NetworkSecurityData"
+DATA_INGESTION_DATABASE_NAME: str = "KNAcd"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = "0.2"
+
+"""
+Data validation related constants start with DATA_VALIDATION VAR name
+"""
+
+
+"""
+Data transformation related constants start with DATA_TRANSFORMATION VAR name
+"""
+
+
+"""
+Model trainer related constants start with MODEL_TRAINER VAR name
+"""
---
+++ networksecurity/entity/config_entity.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import os
+from networksecurity.constant import training_pipeline
+
+print(training_pipeline.ARTIFACT_DIR)
+
+class TrainingPipelineConfig:
+    def __init__(self):
+        pass
+        
+class DataIngestionConfig:
+    def __init__(self, data_ingestion_config):
+        pass
+
+class DataTransformationConfig:
+    def __init__(self):
+        pass
+
+class ModelTrainerConfig:
+    def __init__(self):
+        pass
+
+class ModelEvaluationConfig:
+    def __init__(self):
+        pass
+
+
+        
