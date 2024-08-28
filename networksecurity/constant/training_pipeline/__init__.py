d0926838980556adb5062902449d259623976299
suryanshp1 <suryanshp1@gmail.com>
2024-08-28 23:54:18 +0530

    feat: Added data validation change

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -35,7 +35,11 @@ DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
 """
 Data validation related constants start with DATA_VALIDATION VAR name
 """
-
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 
 """
 Data transformation related constants start with DATA_TRANSFORMATION VAR name
